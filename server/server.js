3af1019a3432328738ea7db69978d039a47f55e9
rohit-raj7 <rohitraj70615@gmail.com>
2025-06-19 07:27:52 +0530

    server

--- server/server.js
+++ server/server.js
@@ -1,47 +1,46 @@
+// server.js or index.js
+
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser'
 import connectDB from './configs/mongodb.js'
 import connectCloudinary from './configs/cloudinary.js'
-import userRouter from './routes/userRoutes.js' 
-import {stripeWebhooks } from './controllers/webhooks.js'
-import {handleUserEvents} from './controllers/webhooks.js'
+
+import userRouter from './routes/userRoutes.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import courseRouter from './routes/courseRoute.js'
-import authRoutes from './routes/authRoutes.js';
+import authRoutes from './routes/authRoutes.js'
 
+import { stripeWebhooks, handleUserEvents } from './controllers/webhooks.js'
 
-// Initialize Express
 const app = express()
 
-// Connect to database
+// Connect to services
 await connectDB()
 await connectCloudinary()
 
-// Middlewares
-// app.use(cors()) 
+// Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend origin
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
-}));
-
+}))
 
 app.use(cookieParser())
+app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => res.send("API Working"))
-app.post('/user-events', express.json() , handleUserEvents)
+app.get('/', (req, res) => res.send('API Working'))
+app.post('/user-events', handleUserEvents)
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
-app.use('/api/educator', express.json(), educatorRouter)
-app.use('/api/course', express.json(), courseRouter)
-app.use('/api/user', express.json(), userRouter)
-app.use('/api/auth', express.json(), authRoutes);
 
+app.use('/api/educator', educatorRouter)
+app.use('/api/course', courseRouter)
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRoutes)
 
-// Port
+// Start server
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`)
 })
