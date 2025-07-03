5bee8f548b107d5c0c12bc6253f76c8b473edb28
rohit-raj7 <rohitraj70615@gmail.com>
2025-07-03 21:56:31 +0530

    gf

--- server cop/utils/generateTokens.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateAccessToken = (userId) => {
-  return jwt.sign({ _id: userId }, process.env.JWT_SECRET, {
-    expiresIn: '15m' // or '1h' based on your design
-  });
-};
